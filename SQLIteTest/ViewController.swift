282233b7beb1f5c13a887b295979c343b6d4ebe3
Alex Reilly <fabiobean2@gmail.com>
2018-11-13 23:15:53 -0800

    Initial Commit

---
+++ SQLIteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLIteTest
+//
+//  Created by New User on 11/13/18.
+//  Copyright © 2018 twof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
